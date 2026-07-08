2dd582bdd2f326bd1149af9d0bddf932062d21c0
fullstack-nick <n.a.petrushin@gmail.com>
2026-07-08 19:01:49 +0200

    Report protocol version 3 in build info

--- tests/build_info_tests.cpp
+++ tests/build_info_tests.cpp
@@ -8,7 +8,7 @@ TEST(BuildInfoTests, CurrentBuildInfoContainsProtocolAndStorageVersions) {
   EXPECT_EQ(info.service, "boltstream");
   EXPECT_FALSE(info.version.empty());
   EXPECT_FALSE(info.git_sha.empty());
-  EXPECT_EQ(info.protocol_version, "2");
+  EXPECT_EQ(info.protocol_version, "3");
   EXPECT_EQ(info.storage_format_version, "2");
 }
 
@@ -17,7 +17,7 @@ TEST(BuildInfoTests, JsonContainsExpectedKeys) {
       boltstream::build_info_json(boltstream::current_build_info(), "2026-07-07T00:00:00Z");
 
   EXPECT_NE(json.find("\"service\":\"boltstream\""), std::string::npos);
-  EXPECT_NE(json.find("\"protocol_version\":\"2\""), std::string::npos);
+  EXPECT_NE(json.find("\"protocol_version\":\"3\""), std::string::npos);
   EXPECT_NE(json.find("\"storage_format_version\":\"2\""), std::string::npos);
   EXPECT_NE(json.find("\"startup_time_utc\":\"2026-07-07T00:00:00Z\""), std::string::npos);
 }
